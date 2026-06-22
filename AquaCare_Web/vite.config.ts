abb5aea0f911b378ffaf37dafe3390e676b7fb31
PHAM LE NHAT MINH <phamlenhatminh1609@gmail.com>
2026-06-22 13:11:58 +0700

    Update vite.config.ts

--- AquaCare_Web/vite.config.ts
+++ AquaCare_Web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/aquacare/',  // Thêm dòng này
+  // base: '/aquacare/',  // Thêm dòng này
   plugins: [
     react(),
     tailwindcss(),
